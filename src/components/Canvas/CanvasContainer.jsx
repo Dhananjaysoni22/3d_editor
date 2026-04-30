45524e3f254f53b28ba34925173e4bc348c79b5b
Dhananjaysoni22 <96926507+Dhananjaysoni22@users.noreply.github.com>
2026-05-01 01:34:02 +0530

    added grid for mode precision

--- src/components/Canvas/CanvasContainer.jsx
+++ src/components/Canvas/CanvasContainer.jsx
@@ -3,6 +3,8 @@ import PointsLayer from "./PointsLayer";
 import SegmentsLayer from "./SegmentsLayer";
 import InteractionManager from "./InteractionManager";
 import ControlPointsLayer from "./ControlPointsLayer";
+import { Grid } from "@react-three/drei";
+
 export default function CanvasContainer() {
   return (
     <Canvas
@@ -10,8 +12,15 @@ export default function CanvasContainer() {
       orthographic
       camera={{ zoom: 50, position: [0, 0, 10] }}
     >
+      <Grid
+        rotation={[Math.PI / 2, 0, 0]}
+        infiniteGrid
+        cellSize={1}
+        sectionSize={3}
+        fadeDistance={50}
+        cellColor="#ffffff"
+      />{" "}
       <color attach="background" args={["#111"]} />
-
       <PointsLayer />
       <SegmentsLayer />
       <ControlPointsLayer />
